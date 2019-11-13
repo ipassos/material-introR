41b630fe7bc449fd1fca6d122e2a396384b5fa80
Iara Passos <iaracpassos@gmail.com>
2019-11-13 12:00:46 -0300

    Add files via upload

---
+++ lista3.R
@@ -0,0 +1,42 @@
+################################################################ LISTA 3 ################################################################
+######################################################## INTRODUÇAO AO R - 2019 #########################################################
+
+################################################################ BLOCO 6 ################################################################ 
+############################################################### TIDYVERSE ###############################################################
+
+### Utilize os pacotes tidyverse para realizar essa lista
+### Atençao para os "NA"
+
+## Banco Titanic
+
+#37 - Filtre os sobreviventes do Titanic, armazene em um novo banco
+
+#38 - Ordene o banco geral (Titanic) pela idade (ordem crescente)
+
+#39 - Ordene pela classe e por ordem decrescente da idade
+
+#40 - Filtre os sobreviventes do sexo masculino e ordene pela classe
+
+#41 - Recodifique a idade dos passageiros em categorias (factor) da forma que desejar (utilize o R base). Crie uma nova coluna com esses resultado (utilizando o dplyr)
+
+#42 - Selecione apenas as colunas classe, sexo e idade em categorias
+
+#43 - Faça uma seleçao excluindo as colunas nome e sexo (categorica)
+
+#44 - Faça um resumo da media das idades
+
+#45 - Calcule o numero de individuos por grupo (sobreviveu/nao sobreviveu)
+
+#46 - Calcule a media de idade dos individuos que sobreviveram por classe
+
+#47 - Descubra qual a pessoa mais velha em cada uma das classes
+
+#48 - Retire os pronomes de tratamento do nome das pessoas
+
+#49 - Altere a variavel sexo categorica para "F" e "M"
+
+#50 - Descubra: Em quais classes as mulheres sobreviveram mais do que os homens?
+
+#51 - Faça um graficos para apresentar a descoberta do exercicio 50
+
+
